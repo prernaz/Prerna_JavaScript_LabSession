bab83ce6797a889ecb3485743154fd98dda4532f
prernaz <98504839+prernaz@users.noreply.github.com>
2022-09-05 19:14:18 +0530

    Add files via upload
    commented version of the file

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-function Quiz(questions){
-    this.score = 0;
-    this.questions = questions;
-    this.questionIndex = 0; 
+    //javascript code for displaying quiz questions and checking and maintaing score in the quiz
+
+    function Quiz(questions){
+        this.score = 0;
+        this.questions = questions;
+        this.questionIndex = 0; 
     }
+    
+    //prototype is used to keep only one copy of functions for all the objects(instances)
 
     Quiz.prototype.getQuestionByIndex = function() {
         return this.questions[this.questionIndex];
     }
 
     Quiz.prototype.checkOptionWithAnswer = function(answer) {
-     if(this.getQuestionByIndex().isCorrectAnswer(answer)){
+        if(this.getQuestionByIndex().isCorrectAnswer(answer)){
             this.score++;
-   }
-   this.questionIndex++;
+        }
+        this.questionIndex++;
    }
 
    Quiz.prototype.isEnded  = function(){
@@ -25,12 +29,11 @@ function Quiz(questions){
       this.answer= answer;
     }
     
-    Question.prototype.isCorrectAnswer = function(choice) 
-    { return this.answer === choice;
+    Question.prototype.isCorrectAnswer = function(choice) { 
+        return this.answer === choice;
     }
 
-
-//create questions here
+    //create set of questions in the quiz with answers
 
     var questions = [
         new Question("JavaScript supports", ["Functions", "XHTML","CSS", "HTML"], "Functions"),
@@ -40,37 +43,23 @@ function Quiz(questions){
         new Question("JavaScript is a ", ["Language", "Programming Language", "Development", "All"], "Programming Language")
     ];
 
-    function showScores(){
-        let scoreHtml = "<h1>Result</h1>";
-        scoreHtml += "<h2 id='score'>Your scores:"+quiz.score+ " </h2>"
-        let quizElement = document.getElementById("quiz");
-        quizElement.innerHTML = scoreHtml;
-      }
-      
-    function showProgressBar(){
-        let currentQuestionNumber = quiz.questionIndex + 1;
-        let progressElement = document.getElementById("progress");
-        progressElement.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
-      }
-      
+         
     let quiz = new Quiz(questions);
     loadQuestions();
 
+    // function to load set of questions one by one in the quiz
     function loadQuestions(){
         if(quiz.isEnded()){
         //show scores
-        showScores();
+            showScores();
         }
         else{
-            // question = getQuestionByIndex
-            //getElementById("question").innerText = question.text
-            // question.choices
 
+        //show options
             let question = quiz.getQuestionByIndex();
             var element = document.getElementById("question");
             element.innerHTML = question.text;
 
-            //show options
             var choices = question.choices;
             for(var i =0; i<choices.length; i++){
                 var element = document.getElementById("choice" + i);
@@ -81,12 +70,28 @@ function Quiz(questions){
         }  
     } 
 
+    //function to handle action to be performed on click/selection  of an option
     function handleOptionButton(id,choice){
         let button = document.getElementById(id);
         button.onclick = function(){
             quiz.checkOptionWithAnswer(choice);
             loadQuestions();
         }
+    }
+
+    //function to keep track of scores and show scores obtained in quiz
+    function showScores(){
+        let scoreHtml = "<h1>Result</h1>";
+        scoreHtml += "<h2 id='score'>Your scores:"+quiz.score+ " </h2>"
+        let quizElement = document.getElementById("quiz");
+        quizElement.innerHTML = scoreHtml;
+    }
+      
+      //function to keep track of progress made in terms of questions attempted at any point in quiz
+    function showProgressBar(){
+        let currentQuestionNumber = quiz.questionIndex + 1;
+        let progressElement = document.getElementById("progress");
+        progressElement.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
     }
 
 
